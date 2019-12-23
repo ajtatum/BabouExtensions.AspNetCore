993108864055db8d8b57587eec8a37b2f83e8983
AJ Tatum <andrew.j.tatum@gmail.com>
2019-12-22 21:19:08 -0500

    Initial commit. Separated from BabouExtensions

---
+++ BabouExtensions.AspNetCore/HttpRequestExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace BabouExtensions.AspNetCore
+{
+    /// <summary>
+    /// Extensions for HttpRequest in AspNetCore
+    /// </summary>
+    public static class HttpRequestExtensions
+    {
+
+        /// <summary>
+        /// Retrieve the raw body as a string from the Request.Body stream
+        /// </summary>
+        /// <param name="request">Request instance to apply to</param>
+        /// <param name="encoding">Optional - Encoding, defaults to UTF8</param>
+        /// <returns></returns>
+        public static async Task<string> GetRawBodyStringAsync(this HttpRequest request, Encoding encoding = null)
+        {
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            using var reader = new StreamReader(request.Body, encoding);
+            return await reader.ReadToEndAsync();
+        }
+
+        /// <summary>
+        /// Retrieve the raw body as a string from the Request.Body stream. Call Request.EnableBuffering() before using this method.
+        /// </summary>
+        /// <param name="request">Request instance to apply to</param>
+        /// <param name="encoding">Optional - Encoding, defaults to UTF8</param>
+        /// <param name="bufferSize">Option - Defaults to -1</param>
+        /// <param name="leaveOpen">Option - Set to true if you need to use the Request Body again</param>
+        /// <returns></returns>
+        public static async Task<string> GetRawBodyStringAsyncWithOptions(this HttpRequest request, Encoding encoding = null, int? bufferSize = null, bool? leaveOpen = null)
+        {
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            if (bufferSize == null)
+                bufferSize = -1;
+
+            if (leaveOpen == null)
+                leaveOpen = false;
+
+            string requestBody;
+
+            using (var reader = new StreamReader(request.Body, encoding: encoding, detectEncodingFromByteOrderMarks: false, bufferSize: bufferSize.Value, leaveOpen: leaveOpen.Value))
+            {
+                requestBody = await reader.ReadToEndAsync();
+                request.Body.Position = 0;
+            }
+            return requestBody;
+        }
+
+        /// <summary>
+        /// Retrieves the raw body as a byte array from the Request.Body stream
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static async Task<byte[]> GetRawBodyBytesAsync(this HttpRequest request)
+        {
+            await using var ms = new MemoryStream(2048);
+            await request.Body.CopyToAsync(ms);
+            return ms.ToArray();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,49 @@
+![alt text](https://raw.githubusercontent.com/ajtatum/BabouExtensions/master/assets/Babou-150x150.png "Babou loves extension methods!") <!-- markdownlint-disable -->
+
+# **Babou Extensions**
+
+[![Build status](https://ci.appveyor.com/api/projects/status/2um6nsg5bub058nv?svg=true)](https://ci.appveyor.com/project/ajtatum/babouextensions) ![Nuget](https://img.shields.io/nuget/dt/BabouExtensions)
+
+Babou Extensions is just another extension method project using .NET Standard 2.0.
+
+## **Extensions**
+
+* DataTables, DataSets
+* DateTime
+* Identity
+* HttpRequests
+* Linq
+* Lists, IEnumerable, and IQueryable
+* Strings
+* Validations and Assertions
+
+## **Install**
+
+For .NET Standard applications, view the NuGet package at https://www.nuget.org/packages/BabouExtensions/
+
+* **Package Manager:** Install-Package BabouExtensions
+* **.NET CLI:** dotnet add package BabouExtensions
+
+For .NET Core applications, view the NuGet package at https://www.nuget.org/packages/BabouExtensions.AspNetCore/
+
+* **Package Manager:** Install-Package BabouExtensions.AspNetCore
+* **.NET CLI:** dotnet add package BabouExtensions.AspNetCore
+
+### **Subscribe**
+
+[Subscribe to Updates via PushBullet](https://www.pushbullet.com/channel?tag=babouextensions)
+
+---
+
+**Be sure to checkout [BabouMail](https://github.com/ajtatum/BabouMail)**
+
+#### Developed by AJ Tatum
+
+[![ajt.io](https://img.icons8.com/clouds/50/000000/domain.png "ajt.io")](https://babou.io/aj)
+[![Facebook @ajtatum](https://img.icons8.com/clouds/50/000000/facebook-new.png "Facebook @ajtatum")](https://babou.io/fbaj)
+[![Twitter @ajtatum](https://img.icons8.com/clouds/50/000000/twitter.png "Twitter @ajtatum")](https://babou.io/twitteraj)
+[![LinkedIn @ajtatum](https://img.icons8.com/clouds/50/000000/linkedin.png "LinkedIn @ajtatum")](https://babou.io/linkedinaj)
+[![Spotify @ajtatum17](https://img.icons8.com/clouds/50/000000/spotify.png "Spotify @ajtatum17")](https://babou.io/spotifyaj)
+[![GitHub @ajtatum](https://img.icons8.com/clouds/50/000000/github.png "GitHub @ajtatum")](https://babou.io/githubaj)
+
+[Icons by Icons8](https://icons8.com/)
